ce2020e387cfd0245b04c7111e728cfe42e7fab8
Jazaltron <jazaltron10@gmail.com>
2021-08-21 08:09:10 +0100

    Adding the Dynamic programming Section now

---
+++ ForLoop.js
@@ -0,0 +1,28 @@
+console.log("Hello world")
+
+for(i = 1; i <= 5; i++){
+    for(j = 1;  j <= i; j++){
+        console.log("+")
+    }               
+    console.log("*")
+}
+
+
+
+for (a = 1; a <= 5; a++)
+{
+    for (y = 1; y <= a; y++)
+        console.log(" * ");
+    // cout << endl;
+}
+
+
+for(let  i  = 0 ; i < 100; )
+    console.log((++i % 3 ? "" : "fizz") + (i % 5 ? "" : "buzz") || i);
+
+
+
+
+
+
+
